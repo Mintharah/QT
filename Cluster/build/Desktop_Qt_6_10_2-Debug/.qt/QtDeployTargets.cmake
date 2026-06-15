181e2aed0cd6094045033d81bbbb525fbade5737
Yasmine Elkady <yasmine.elkady333@gmail.com>
2026-06-15 17:48:56 +0300

    Delete Cluster/build/Desktop_Qt_6_10_2-Debug directory

--- Cluster/build/Desktop_Qt_6_10_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appCluster_FILE /home/yasmine/Cluster/build/Desktop_Qt_6_10_2-Debug/appCluster)
-set(__QT_DEPLOY_TARGET_appCluster_TYPE EXECUTABLE)
--- Cluster/build/Desktop_Qt_6_10_2-Debug/.qt/appCluster_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appCluster")
-set(working_dir "/home/yasmine/Cluster")
-set(src_and_dest_list
-    "/home/yasmine/Cluster/Main.qml"
-    "/home/yasmine/Cluster/build/Desktop_Qt_6_10_2-Debug/Cluster/Main.qml"
-
-)
-set(timestamp_file "/home/yasmine/Cluster/build/Desktop_Qt_6_10_2-Debug/.qt/appCluster_qml.txt")
