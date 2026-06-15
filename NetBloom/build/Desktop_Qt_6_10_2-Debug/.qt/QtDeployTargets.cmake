c483e784d4551e3661f4820b66c6133eb219dc0d
Yasmine Elkady <yasmine.elkady333@gmail.com>
2026-06-15 14:13:03 +0300

    Delete NetBloom/build/Desktop_Qt_6_10_2-Debug directory

--- NetBloom/build/Desktop_Qt_6_10_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appNetBloom_FILE /home/yasmine/NetBloom/build/Desktop_Qt_6_10_2-Debug/appNetBloom)
-set(__QT_DEPLOY_TARGET_appNetBloom_TYPE EXECUTABLE)
--- NetBloom/build/Desktop_Qt_6_10_2-Debug/.qt/appNetBloom_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appNetBloom")
-set(working_dir "/home/yasmine/NetBloom")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/yasmine/NetBloom/build/Desktop_Qt_6_10_2-Debug/.qt/appNetBloom_res.txt")
