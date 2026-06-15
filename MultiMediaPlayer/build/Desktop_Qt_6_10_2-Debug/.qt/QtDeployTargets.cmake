e089e924c08e759b05207ef53da0bbc15e498b22
Yasmine Elkady <yasmine.elkady333@gmail.com>
2026-06-15 14:03:07 +0300

    Delete MultiMediaPlayer/build/Desktop_Qt_6_10_2-Debug directory

--- MultiMediaPlayer/build/Desktop_Qt_6_10_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appMultiMediaPlayer_FILE /home/yasmine/MultiMediaPlayer/build/Desktop_Qt_6_10_2-Debug/appMultiMediaPlayer)
-set(__QT_DEPLOY_TARGET_appMultiMediaPlayer_TYPE EXECUTABLE)
--- MultiMediaPlayer/build/Desktop_Qt_6_10_2-Debug/.qt/appMultiMediaPlayer_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appMultiMediaPlayer")
-set(working_dir "/home/yasmine/MultiMediaPlayer")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/yasmine/MultiMediaPlayer/build/Desktop_Qt_6_10_2-Debug/.qt/appMultiMediaPlayer_res.txt")
