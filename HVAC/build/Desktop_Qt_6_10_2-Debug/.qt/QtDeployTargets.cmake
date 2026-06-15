282fab8b53fc8c51f7081b2fa97ff41108328e87
Yasmine Elkady <yasmine.elkady333@gmail.com>
2026-06-15 14:18:47 +0300

    Delete HVAC/build/Desktop_Qt_6_10_2-Debug directory

--- HVAC/build/Desktop_Qt_6_10_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appHVAC_FILE /home/yasmine/HVAC/build/Desktop_Qt_6_10_2-Debug/appHVAC)
-set(__QT_DEPLOY_TARGET_appHVAC_TYPE EXECUTABLE)
--- HVAC/build/Desktop_Qt_6_10_2-Debug/.qt/appHVAC_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appHVAC")
-set(working_dir "/home/yasmine/HVAC")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/yasmine/HVAC/build/Desktop_Qt_6_10_2-Debug/.qt/appHVAC_res.txt")
